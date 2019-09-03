6dba97d5cb5039df73d3e5653973485b77f25a5d
Val <valerysehbai@gmail.com>
2019-09-03 14:49:59 +0200

    stuck on exercise3

--- band-name-solution-3.js
+++ band-name-solution-3.js
@@ -14,8 +14,28 @@
  * 
  *  generateBandName('RED', 'cookies')
  */
-const bandNames = [];
+// const bandNames = [];
 
+// function generateBandName(clothingColor, lastFoodEaten) {
+//   // format the input data and store it for later
+
+//   const color = clothingColor[0].toUpperCase() + clothingColor.substr(1).toLowerCase();
+//   const food = lastFoodEaten[0].toUpperCase() + lastFoodEaten.substr(1).toLowerCase();
+//   bandNames.push(`The ${color} ${food}`);
+//   // use the formatted inputs to build the return
+//   return `The ${color} ${food}!`;
+
+// }
+
+// console.log(generateBandName('blACK', 'spAGhetti'));
+// console.log(generateBandName('purple', 'assholes'))
+// console.log(generateBandName('pink', 'elephants'))
+// console.log(generateBandName('yeLLOw', 'tORTELLIni'))
+// console.log(bandNames);
+
+
+//exercise 3 : call the same function twice , same result  
+//const bandNames = [];
 function generateBandName(clothingColor, lastFoodEaten) {
   // format the input data and store it for later
 
@@ -30,6 +50,5 @@ function generateBandName(clothingColor, lastFoodEaten) {
 console.log(generateBandName('blACK', 'spAGhetti'));
 console.log(generateBandName('purple', 'assholes'))
 console.log(generateBandName('pink', 'elephants'))
-//bandName = generateBandName();
-//bandNames.push(bandName);
+console.log(generateBandName('yeLLOw', 'tORTELLIni'))
 console.log(bandNames);
